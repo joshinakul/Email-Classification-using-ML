9fb2dd52e3819088953b8fa6677dd79bc9314e6b
joshinakul <nakulj3010@outlook.com>
2019-03-01 21:39:26 +0530

    first commit

---
+++ text_preprocess.py
@@ -0,0 +1,21 @@
+import os
+from collections import Counter
+
+direc = "emails/"
+files = os.listdir(direc)
+
+emails = [direc + email for email in files]
+
+words = []
+c = len(emails)
+
+for email in emails:
+    f = open(email)
+    blob = f.read()
+    words += blob.split(" ")
+    print (c)
+    c -= 1
+    
+print (words)
+    
+    
